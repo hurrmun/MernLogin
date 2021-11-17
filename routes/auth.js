e04a31d25e180e731d86d62fa31e2524f2b81858
hurrmun <61788143+hurrmun@users.noreply.github.com>
2021-11-17 13:11:32 +0800

    added routes and controllers for login and register, private for authentication, auth for creating acc and login

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express();
+
+const {
+  register,
+  login,
+  forgotpassword,
+  resetpassword,
+} = require("../controllers/auth");
+
+router.route("/register").post(register);
+//? router.post("/register", register)
+
+router.route("/login").post(login);
+
+router.route("/forgotpassword").post(forgotpassword);
+
+router.route("/resetpassword/:resetToken").put(resetpassword);
+
+module.exports = router;
