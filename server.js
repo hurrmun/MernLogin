c863f041027a69af31a5e156e5805805ce1d9a21
hurrmun <61788143+hurrmun@users.noreply.github.com>
2021-11-17 13:09:37 +0800

    setup for authentication login app

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error.js");
+
+//* Connect to DB
+connectDB();
+
+//* Middleware
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+
+//* Error Handler Middleware (should be last piece of middlware)
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+  console.log("listening on port: ", PORT);
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1));
+});
