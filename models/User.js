442ed8b8a00eacc36e35164c351416131d4bcc19
hurrmun <61788143+hurrmun@users.noreply.github.com>
2021-11-18 10:26:39 +0800

    fixed error in password portion of model that was not saving on the database

--- models/User.js
+++ models/User.js
@@ -16,15 +16,15 @@ const UserSchema = new mongoose.Schema({
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide a valid email",
     ],
-    password: {
-      type: String,
-      required: [true, "Please add a password"],
-      minlength: 6,
-      select: false,
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
   },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
 
 //* Middleware to hash password
